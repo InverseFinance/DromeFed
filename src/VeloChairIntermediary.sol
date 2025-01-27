e9645bbf492f333d2b1585f358ac6ad8894e3dae
08xmt <markus.08xmt@gmail.com>
2025-01-27 12:56:46 +0100

    Remove native functions from VeloChairIntermediary

--- src/VeloChairIntermediary.sol
+++ src/VeloChairIntermediary.sol
@@ -5,14 +5,10 @@ interface IVeloFarmer {
     function withdrawLiquidity(uint dolaAmount) external;
     function withdrawLiquidityAndSwapToDOLA(uint dolaAmount) external;
     function withdrawToL1OptiFed(uint dolaAmount) external;
-    function withdrawToL1OptiFedBridged(uint usdcAmount) external;
+    function withdrawToL1OptiFed(uint dolaAmount, uint usdcAmount) external;
     function withdrawTokensToL1(address l2Token, uint amount) external;
     function swapUSDCtoDOLA(uint usdcAmount) external;
-    function swapUSDCNativetoDOLA(uint usdcAmount) external;
     function swapDOLAtoUSDC(uint dolaAmount) external;
-    function swapDOLAtoUSDCNative(uint dolaAmount) external;
-    function swapUSDCtoUSDCNative(uint usdcAmount) external;
-    function swapUSDCNativeToUSDC(uint usdcAmount) external;
     function resign() external;
 }
 
@@ -42,8 +38,8 @@ contract VeloChairIntermediary {
         veloFarmer.withdrawToL1OptiFed(dolaAmount);
     }
 
-    function withdrawToL1OptiFedBridged(uint usdcAmount) external onlyChair {
-        veloFarmer.withdrawToL1OptiFedBridged(usdcAmount);
+    function withdrawToL1OptiFed(uint dolaAmount, uint usdcAmount) external onlyChair {
+        veloFarmer.withdrawToL1OptiFed(dolaAmount, usdcAmount);
     }
 
     function withdrawTokensToL1(address l2Token, uint amount) external onlyChair{
@@ -54,24 +50,8 @@ contract VeloChairIntermediary {
         veloFarmer.swapUSDCtoDOLA(usdcAmount);
     }
 
-    function swapUSDCNativetoDOLA(uint usdcAmount) external onlyChair {
-        veloFarmer.swapUSDCNativetoDOLA(usdcAmount);
-    }
-
     function swapDOLAtoUSDC(uint dolaAmount) external onlyChair {
         veloFarmer.swapDOLAtoUSDC(dolaAmount);
-    }
-
-    function swapDOLAtoUSDCNative(uint dolaAmount) external onlyChair {
-        veloFarmer.swapDOLAtoUSDCNative(dolaAmount);
-    }
-
-    function swapUSDCtoUSDCNative(uint usdcAmount) external onlyChair {
-        veloFarmer.swapUSDCtoUSDCNative(usdcAmount);
-    }
-
-    function swapUSDCNativeToUSDC(uint usdcAmount) external onlyChair {
-        veloFarmer.swapUSDCNativeToUSDC(usdcAmount);
     }
 
     function resign() external onlyChair {
